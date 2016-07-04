d689b18e3f89a3b255b953330e648981043835df
Rob Coward <rob@jive-videos.net>
2016-07-04 21:55:07 +0100

    Removed find loop so that the script can be called with a single filepath

--- mkv_convert.sh
+++ mkv_convert.sh
@@ -1,54 +1,86 @@
 #!/bin/bash
 #
 
-VIDEOS_DIR=$1
+IN_FILE=$1
+
+SOURCE_FILE=$(basename $IN_FILE)
+MP4_FILE="$(basename $IN_FILE mkv)mp4"
 
 outformat=mp4
 
-find ${VIDEOS_DIR} -name "*mkv" -size +0 | while read to_convert 
-do
-  SOURCE_DIR=$(dirname $to_convert)
-  SOURCE_FILE=$(basename $to_convert)
-  MP4_FILE="$(basename $to_convert mkv)mp4"
-  cd ${SOURCE_DIR}
-  if [ ! -f ${MP4_FILE} ];
-  then
+# Check FFMPEG Installation
+if ffmpeg -formats > /dev/null 2>&1 
+then
+  ffversion=`ffmpeg -version 2> /dev/null | grep ffmpeg | sed -n 's/ffmpeg\s//p'`
+  echo "Your ffmpeg verson is $ffversion"
+else
+  echo "ERROR: You need ffmpeg installed with x264 and libfdk_aac encoder"
+  exit
+fi
 
-    if ffmpeg -i ${SOURCE_FILE} 2>&1 | grep 'Invalid data found'		#check if it's video file
-    then
-      echo "ERROR File ${SOURCE_FILE} is NOT A VIDEO FILE can be converted!"
-      continue	   	
-    fi
+if ffmpeg -formats 2> /dev/null | grep "E mp4" > /dev/null
+then
+  echo "Check mp4 container format ... OK"
+else
+  echo "Check mp4 container format ... NOK"
+  exit
+fi
 
-    if ffmpeg -i ${SOURCE_FILE} 2>&1 | grep Video: | grep h264		#check video codec
-    then
-      vcodec=copy
-    else
-      vcodec=libx264
-    fi
+if ffmpeg -formats 2> /dev/null | grep "E matroska" > /dev/null
+then
+  echo "Check mkv container format ... OK"
+else
+  echo "Check mkv container format ... NOK"
+  exit
+fi
 
-    if ffmpeg -i ${SOURCE_FILE} 2>&1 | grep Video: | grep "High 10"	#10 bit H.264 can't be played by Hardware.
-    then
-      vcodec=libx264
-    fi
+if ffmpeg -codecs 2> /dev/null | grep "libfdk_aac" > /dev/null
+then
+  echo "Check AAC Audio Encoder ... OK"
+else
+  echo "Check AAC Audio Encoder ... NOK"
+  exit
+fi
 
-    if [ ffmpeg -i ${SOURCE_FILE} 2>&1 | grep Audio: | grep aac ] || [ 	ffmpeg -i ${SOURCE_FILE} 2>&1 | grep Audio: | grep mp3 ]	#check audio codec
-    then
-      acodec=copy
-    else
-      acodec=libfdk_aac
-    fi
+if ffmpeg -codecs 2> /dev/null | grep "libx264" > /dev/null
+then
+  echo "Check x264 the free H.264 Video Encoder ... OK"
+else
+  echo "Check x264 the free H.264 Video Encoder ... NOK"
+  exit
+fi
 
-    echo "Converting ${SOURCE_FILE}"
-    echo "Video codec: $vcodec Audio codec: $acodec Container: $outformat" 
 
-    # using ffmpeg for real converting
-    echo "ffmpeg -i ${SOURCE_FILE} -y -f $outformat -acodec $acodec -ab 192k -ac 2 -absf aac_adtstoasc -async 1 -vcodec $vcodec -vsync 0 -profile:v main -level 3.1 -qmax 22 -qmin 20 -x264opts no-cabac:ref=2 -threads 0 ${MP4_FILE}"
-    ffmpeg -i ${SOURCE_FILE} -y -f $outformat -acodec $acodec -ab 192k -ac 2 -absf aac_adtstoasc -async 1 -vcodec $vcodec -vsync 0 -profile:v main -level 3.1 -qmax 22 -qmin 20 -x264opts no-cabac:ref=2 -threads 0 ${MP4_FILE}
+if ffmpeg -i ${SOURCE_FILE} 2>&1 | grep 'Invalid data found'		#check if it's video file
+then
+  echo "ERROR File ${SOURCE_FILE} is NOT A VIDEO FILE can be converted!"
+  exit	   	
+fi
 
-    # Replace the original mkv file with an empty placeholder to prevent it being processed again
-    echo -n > ${to_convert}
-  else
-    echo "WARN: Both ${SOURCE_FILE} and ${MP4_FILE} exist in ${SOURCE_DIR}"
-  fi
-done
+if ffmpeg -i ${SOURCE_FILE} 2>&1 | grep Video: | grep h264		#check video codec
+then
+  vcodec=copy
+else
+  vcodec=libx264
+fi
+
+if ffmpeg -i ${SOURCE_FILE} 2>&1 | grep Video: | grep "High 10"	#10 bit H.264 can't be played by Hardware.
+then
+  vcodec=libx264
+fi
+
+if [ ffmpeg -i ${SOURCE_FILE} 2>&1 | grep Audio: | grep aac ] || [ 	ffmpeg -i ${SOURCE_FILE} 2>&1 | grep Audio: | grep mp3 ]	#check audio codec
+then
+  acodec=copy
+else
+  acodec=libfdk_aac
+fi
+
+echo "Converting ${SOURCE_FILE}"
+echo "Video codec: $vcodec Audio codec: $acodec Container: $outformat" 
+
+# using ffmpeg for real converting
+echo "ffmpeg -i ${SOURCE_FILE} -y -f $outformat -acodec $acodec -ab 192k -ac 2 -absf aac_adtstoasc -async 1 -vcodec $vcodec -vsync 0 -profile:v main -level 3.1 -qmax 22 -qmin 20 -x264opts no-cabac:ref=2 -threads 0 ${MP4_FILE}"
+
+ffmpeg -i ${SOURCE_FILE} -y -f $outformat -acodec $acodec -ab 192k -ac 2 -absf aac_adtstoasc -async 1 -vcodec $vcodec -vsync 0 -profile:v main -level 3.1 -qmax 22 -qmin 20 -x264opts no-cabac:ref=2 -threads 0 ${MP4_FILE}
+
